6ba134d73c9dba544baa5448b56e823ffdb64934
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-12-25 10:40:41 +0100

    ajout lsp

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -40,9 +40,17 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
+  -- general
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use {
+      'numToStr/Comment.nvim',
+      config = function()
+          require('Comment').setup()
+      end
+  }
+
   -- colorschemes
   use 'navarasu/onedark.nvim'
   use 'folke/tokyonight.nvim'
@@ -50,6 +58,23 @@ return packer.startup(function(use)
   use 'shaunsingh/nord.nvim'
   use 'dracula/vim'
   use 'tomasiser/vim-code-dark'
+
+  -- completion
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  --
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
