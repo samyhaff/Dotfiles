14f256fc2f8031b1ef74d2c89142babb47394f00
samyhaff <haffoudhisamy@gmail.com>
2023-03-09 15:03:57 +0100

    added plugins

--- nvim/.config/nvim/lua/config/packer.lua
+++ nvim/.config/nvim/lua/config/packer.lua
@@ -2,41 +2,33 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-
+  use 'lewis6991/gitsigns.nvim'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use 'tpope/vim-fugitive'
   use 'nvim-tree/nvim-web-devicons'
-
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-
   use 'tomasiser/vim-code-dark'
-
   use {
     'numToStr/Comment.nvim',
     config = function()
         require('Comment').setup()
     end
   }
-
   use {
   'nvim-lualine/lualine.nvim',
   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
-
   use {'akinsho/bufferline.nvim', requires = 'nvim-tree/nvim-web-devicons'}
-
   use {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
-
   use 'adelarsq/neofsharp.vim'
-
   use 'kyazdani42/nvim-tree.lua'
-
   use "windwp/nvim-autopairs"
-
   use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v1.x',
